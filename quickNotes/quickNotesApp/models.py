79cdb3a7f4b44a6d37d03d251ffe13d61f330c28
Adil-Bikiev <adil.bikiev1@gmail.com>
2026-03-07 22:43:55 -0600

    add 2nd commit

--- quickNotes/quickNotesApp/models.py
+++ quickNotes/quickNotesApp/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notes")
+
+    class Meta:
+        ordering = ["-updated_at"]
+
+    def __str__(self):
+        return self.title
--- quickNotes/quickNotesApp/views.py
+++ quickNotes/quickNotesApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "content"]
+
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
